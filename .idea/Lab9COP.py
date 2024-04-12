cc0abf8f7a5a5408e6f029523e73c312ca207450
maddybd22 <166658864+maddybd22@users.noreply.github.com>
2024-04-12 17:59:14 -0400

    Update Lab9COP.py

--- .idea/Lab9COP.py
+++ .idea/Lab9COP.py
@@ -23,7 +23,8 @@ if __name__ == '__main__':
         option = input('Please enter an option')
         if option == '1':
             password = int(input('Please enter your password to encode:'))
-            print(encode(password))
+            encode(password)
+            print('Your password has been encoded and stored!')
         if option == '2':
             print(f'The encoded password is {encode(password)}, and the original password is {decode(encode(password))}. ')
         if option == '3':
